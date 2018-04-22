8ad721439698f919dce3e3832104455d2455ce10
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-04-22 16:10:17 +0900

    コメント追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,7 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <meta name="description" content="ヤンマー特約店、齊敏機械　青森県　西津軽郡沿岸部　深浦町　を拠点として活動、YANMARマリン事業全般（FRP船、プレジャーボート、マリンエンジン、海洋設備）に対応しています。お気軽にどうぞお立ち寄り下さい。皆様のお越しを社員一同お待ちしております。" />
+        <meta name="keywords" content=“船,マリンプレジャー,販売,FRP,プレジャーボート,マリンエンジン,ディーゼルエンジン,海洋設備,齊敏機械,青森,深浦町,ヤンマー,ヤンマー特約店” />
+
+        <title>ヤンマー特約店／青森県西津軽深浦町／齊敏機械／ヤンマー舶用製品、マリンプレジャーの販売・整備・修理・加工</title>
         
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -14,8 +17,7 @@
     </head>
     <body>
         <div>
-            <div>Laravel</div>
-            <a href="/mc/lineup/">ラインナップ トップ</a>     
+            aaaa
         </div>
     </body>
 </html>
