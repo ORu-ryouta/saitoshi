b98c126447dd1010bcd48b44319dfdd8839c0445
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-04-22 14:14:42 +0900

    コメント追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -81,15 +81,10 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                
+                <a href="/mc/lineup/">ラインナップ トップ</a> 
+             
         </div>
     </body>
 </html>
+
