bfd21a213d92200ae7fd840634080fce37ed84d4
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-05-19 12:42:30 +0900

    文字追加

--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -1,42 +1,96 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>adminLTE test</title>
+    <!-- for responsive -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- bootstrap -->
+    <link href="{{asset("bower_components/admin-lte/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet" type="text/css" />
+    <!-- font awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- ionicons -->
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- adminLTE style -->
+    <link href="{{asset("bower_components/admin-lte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset("bower_components/admin-lte/dist/css/skins/skin-blue.min.css")}}" rel="stylesheet" type="text/css" />
+</head>
+<body class="skin-blue">
+    <div class="wrapper">
 
-        <meta name="description" content="ヤンマー特約店、齊敏機械　青森県　西津軽郡沿岸部　深浦町　を拠点として活動、YANMARマリン事業全般（FRP船、プレジャーボート、マリンエンジン、海洋設備）に対応しています。お気軽にどうぞお立ち寄り下さい。皆様のお越しを社員一同お待ちしております。" />
-        <meta name="keywords" content=“船,マリンプレジャー,販売,FRP,プレジャーボート,マリンエンジン,ディーゼルエンジン,海洋設備,齊敏機械,青森,深浦町,ヤンマー,ヤンマー特約店” />
+        <!-- トップメニュー -->
+        <header class="main-header">
 
-        <title>ヤンマー特約店／青森県西津軽深浦町／齊敏機械／ヤンマー舶用製品、マリンプレジャーの販売・整備・修理・加工</title>
+            <!-- ロゴ -->
+            <a href="" class="logo">管理画面</a>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles -->
-
-    </head>
-    <body>
-        <div>
-            <h1>
-                <a href=“http://homestead.app">
-                    <img src="{{ asset('/img/saitoshi_logo.png') }}" width="100%" height="100%" alt="ヤンマー特約店｜青森県深浦町｜齊敏機械｜ヤンマー舶用製品、マリンプレジャーの販売・整備・修理・加工" />
-                </a>
-            </h1>
-            <div>
-                <ul>
-                    <li>株式会社　SRM ホールディングス　本社　090−7076−2681</li>
-                    <li>齊敏機械　0173-76-2681</li>
+            <!-- トップメニュー -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li><a href="">顧客管理</a></li>
                 </ul>
-            </div>
-        </div>
-        
-        
-        
-        
-        
-        
-        
-        
-    </body>
-</html>
+            </nav>
 
+        </header><!-- end header -->
+
+
+        <!-- サイドバー -->
+        <aside class="main-sidebar">
+            <section class="sidebar">
+                <ul class="sidebar-menu">
+
+                    <!-- メニューヘッダ -->
+                    <li class="header">機能一覧</li>
+
+                    <!-- メニュー項目 -->
+                    <li><a href="">新規登録</a></li>
+
+                </ul>
+            </section>
+        </aside><!-- end sidebar -->
+
+
+        <!-- content -->
+        <div class="content-wrapper">
+
+            <!-- コンテンツヘッダ -->
+            <section class="content-header">
+                <h1>ページタイトル</h1>
+            </section>
+
+            <!-- メインコンテンツ -->
+            <section class="content">
+
+                <!-- コンテンツ1 -->
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">ボックスタイトル</h3>
+                    </div>
+                    <div class="box-body">
+                        <p>ボックスボディー</p>
+                    </div>
+                </div>
+
+            </section>
+
+        </div><!-- end content -->
+
+
+        <!-- フッター -->
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">Version1.0</div>
+            <strong>Copyright &copy; 2015</strong>, All rights reserved.
+        </footer><!-- end footer -->
+
+
+    </div><!-- end wrapper -->
+    <!-- JS -->
+
+    <!-- jquery -->
+    <script src="{{asset("bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js")}}" type="text/javascript"></script>
+    <!-- bootstrap -->
+    <script src="{{asset("bower_components/admin-lte/bootstrap/js/bootstrap.min.js")}}" type="text/javascript"></script>
+    <!-- adminLTE -->
+    <script src="{{asset("bower_components/admin-lte/dist/js/app.min.js")}}" type="text/javascript"></script>
+</body>
+</html>
