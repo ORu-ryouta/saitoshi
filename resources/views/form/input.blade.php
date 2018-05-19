2335bab9bb4440332fd0aff3f25e2d1c4a9cc656
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-05-19 23:52:39 +0900

    入力画面修正

--- resources/views/form/input.blade.php
+++ resources/views/form/input.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.common')
 @section('content')
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 <div class="container">
     <h3>Laravelで簡単なフォームを作ってみる</h3>
  
