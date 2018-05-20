170918e813226d5f59616b0ff72482b1ef885789
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-05-20 12:50:19 +0900

    エラーメッセージ修正

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -27,8 +27,8 @@ class PostRequest extends FormRequest
             'name' => 'required|max:100',
             'gender'=>'required|max:100',
             'adles'=>'required|max:100',
-            'tel_1' => 'required|max:255',
-            'tel_2' => 'required|max:255',
+            'tel1' => 'required|max:255',
+            'tel2' => 'required|max:255',
             'mail' => 'required',
             'company' => 'required|max:100',
             // 'ship_id' => 'required'
@@ -43,16 +43,16 @@ class PostRequest extends FormRequest
             'gender.in' => '性別を正しく選択してください。',
             'adles.required'=>'住所を正しく入力してください。',
             'adles.max'=>'住所は指定文字数以内で入力してください。',
-            'tel_1.required' => '電話番号を入力してください。',
-            'tel_1.max' => '電話番号は指定文字数以内で入力してください。',
-            'tel_2.required' => '電話番号を入力してください。',
-            'tel_2.max' => '電話番号は指定文字数以内で入力してください。',
+            'tel1.required' => '電話番号を入力してください。',
+            'tel1.max' => '電話番号は指定文字数以内で入力してください。',
+            'tel2.required' => '電話番号を入力してください。',
+            'tel2.max' => '電話番号は指定文字数以内で入力してください。',
             'mail.required' => 'メールアドレスを入力してください。',
             'mail.email' => 'メールアドレスは正しいメールアドレス形式で入力してください。',
             'company.required'=>'会社名を入力してください。',
             'company.max' => '会社名は指定文字数以内で入力してください。',
-            'ship_id.required'=>'船舶名を入力してください。',
-            'ship_id.max'=>'船舶名は指定文字数以内で入力してください。',
+            'shipId.required'=>'船舶名を入力してください。',
+            'shipId.max'=>'船舶名は指定文字数以内で入力してください。',
         ];
     }
 }
