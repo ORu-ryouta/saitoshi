4271c78a66c5529a85fab0dd3757a027bea35330
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-06 21:46:19 +0900

    memberslist

--- resources/views/form/membersList.blade.php
+++ resources/views/form/membersList.blade.php
@@ -1,6 +1,22 @@
 @extends('adminlte.layout')
 @section('content')
 <div class="container">
+  <table>
+    <tr>
+      <th>顧客名</th>
+      <th>性別</th>
+      <th>住所</th>
+      <th>連絡先１</th>
+      <th>連絡先２</th>
+      <th>E-mail</th>
+    </tr>
+
+
+
+
+
+
+  </table>
 	<ul>
     	@foreach($data as $d)
       		<li>{{$d->name}}</li>
