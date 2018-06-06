b148d7a4988e3062e3b0b57a800427fe05fac081
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-06 21:14:46 +0900

    member Table 内容」追加

--- resources/views/form/membersList.blade.php
+++ resources/views/form/membersList.blade.php
@@ -4,6 +4,11 @@
 	<ul>
     	@foreach($data as $d)
       		<li>{{$d->name}}</li>
+      		<li>{{$d-gender}}</li>
+      		<li>{{$d->address}}</li>
+      		<li>{{$d->tel_1}}</li>
+      		<li>{{$d->tel_2}}</li>
+      		<li>{{$d->email}}</li>
     	@endforeach
   	</ul>
 </div>
