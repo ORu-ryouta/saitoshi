1ae9976a7d10d6816ead79540ce1e427ca4a2c75
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-07 23:48:57 +0900

    内容追加

--- resources/views/form/membersList.blade.php
+++ resources/views/form/membersList.blade.php
@@ -11,34 +11,37 @@
       <th>E-mail</th>
     </tr>
       @foreach($data as $d)
-      <tr>
-          <th>{{$d->name}}</th>
-          <th>{{$d->gender}}</th>
-          <th>{{$d->address}}</th>
-          <th>{{$d->tel_1}}</th>
-          <th>{{$d->tel_2}}</th>
-          <th>{{$d->email}}</th>
-      </tr>
+    <tr>
+      <th>{{$d->name}}</th>
+      <th>{{$d->gender}}</th>
+      <th>{{$d->address}}</th>
+      <th>{{$d->tel_1}}</th>
+      <th>{{$d->tel_2}}</th>
+      <th>{{$d->email}}</th>
+      
+    <tr><button type="submit" class="btn btn-primary">編集</button></tr>
+           
+    </tr>
       @endforeach
       <div class="form-group row">
             <div class="offset-sm-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">更新する</button>
             </div>
       </div>
-      
-      <div class="form-group row">
+  </table>
+   
+    <div class="form-group row">
             <div class="offset-sm-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">削除する</button>
             </div>
        </div>
- <form method="GET" action="{{ route('form::input') }}">
+  
+    <form method="GET" action="{{ route('form::input') }}">
         <div class="form-group row">
             <div class="offset-sm-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">新規登録</button>
             </div>
         </div>
     </form>
-  </table>
-	
-</div>
+</div>１
 @endsection
