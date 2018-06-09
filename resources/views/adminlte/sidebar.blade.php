e81832136665aa0977e6d4204fb63923547c6baf
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-09 22:24:25 +0900

    sidebar blade 内容追加

--- resources/views/adminlte/sidebar.blade.php
+++ resources/views/adminlte/sidebar.blade.php
@@ -7,7 +7,7 @@
                     <li class="header">機能一覧</li>
 
                     <!-- メニュー項目 -->
-                    <li><a href="">新規登録</a></li>
+                    <li><a href="{{ action('FormController@memberList') }}">顧客管理</a></li>
 
                 </ul>
             </section>
