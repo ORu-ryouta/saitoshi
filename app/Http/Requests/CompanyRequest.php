a6c30371c8eecd6a85de84997e9553cd49f4c51b
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-10 22:34:36 +0900

    内容追加

--- app/Http/Requests/CompanyRequest.php
+++ app/Http/Requests/CompanyRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CompanyRequest extends FormRequest
+class CompanyRequest extends CompanyRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- resources/views/adminlte/footer.blade.php
+++ resources/views/adminlte/footer.blade.php
@@ -1,5 +1,5 @@
     <!-- フッター -->
         <footer class="main-footer">
             <div class="pull-right hidden-xs">Version1.0</div>
-            <strong>Copyright &copy; 2015</strong>, All rights reserved.
+            <strong>Copyright &copy; SRMホールディングス2018</strong>, All rights reserved.
         </footer><!-- end footer -->
