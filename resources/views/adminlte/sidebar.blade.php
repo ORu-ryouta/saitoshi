e7fd2808b5735a5b7c58b2ecf90f9ce1b2796445
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-10 14:19:45 +0900

    サイドバー修正

--- resources/views/adminlte/sidebar.blade.php
+++ resources/views/adminlte/sidebar.blade.php
@@ -7,9 +7,9 @@
                     <li class="header">機能一覧</li>
 
                     <!-- メニュー項目 -->
-                    <li><a href="{{ action('FormController@membersList') }}">顧客管理</a></li>
+                    <li><a href="{{ action('MemberController@memberList') }}">顧客管理</a></li>
 
-                    <li><a href="{{ action('FormController@membersList') }}">顧客管理</a></li>
+                    <li><a href="{{ action('MemberController@memberList') }}">顧客管理</a></li>
                 </ul>
             </section>
         </aside><!-- end sidebar -->
