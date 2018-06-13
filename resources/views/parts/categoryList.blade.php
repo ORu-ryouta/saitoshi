90799cbc418346eb06ebbe5d114d2804bcc2e87d
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-13 20:25:05 +0900

    categoryListから編集と削除を削除

--- resources/views/parts/categoryList.blade.php
+++ resources/views/parts/categoryList.blade.php
@@ -22,29 +22,7 @@
             </div>
         </form>
     </th> 
-    <th>
-        <form method="GET" action="{{ route('parts::input') }}">
-            <input type="hidden" class="form-control" name="partsId" value="{{$d->parts_id}}">
-            <div class="form-group row">
-                <div class="offset-sm-2 col-sm-10">
-                    <button type="submit" class="btn btn-primary">編集</button>
-                </div>
-            </div>
-        </form>
-    </th>
-    <th>
-        <form method="GET" action="{{ route('parts::delete') }}">
-            <input type="hidden" class="form-control" name="partsId" value="{{$d->parts_id}}">
-            <div class="form-group row">
-                <div class="offset-sm-2 col-sm-10">
-                    <button type="submit" onclick="return submitcheck();">削除</button>
-                </div>
-            </div>
-        </form>
-    </th>
-        
     
-    </tr>
       @endforeach
       @endif
   </table>
@@ -57,11 +35,5 @@
     </form>
 </div>
 
-<script  type="text/javascript">
-//    削除ダイアログ
-function submitcheck() {
-	var check = confirm('削除？');
-	return check;
-}
-</script>
+
 @endsection
