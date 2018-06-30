d1a0406df4d4497a4a6770a140a005fb8d271c63
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-06-30 20:32:39 +0900

    内容

--- resources/views/demand/input.blade.php
+++ resources/views/demand/input.blade.php
@@ -81,7 +81,7 @@
         </div>
     </form>
     <from method="GET" action="{{ route('sale::input') }}">
-    <div class="form-group row">
+        <div class="form-group row">
             <div class="offset-sm-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">売り上げ登録</button>
             </div>
