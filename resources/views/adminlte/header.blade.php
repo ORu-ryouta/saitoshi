cecfc4b0021d13019df3e9d09e6206d895a21b08
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-07-02 00:25:14 +0900

    ヘッダー　内容追加

--- resources/views/adminlte/header.blade.php
+++ resources/views/adminlte/header.blade.php
@@ -7,7 +7,8 @@
             <!-- トップメニュー -->
             <nav class="navbar navbar-static-top" role="navigation">
                 <ul class="nav navbar-nav">
-                    <li><a href="www.yanmar.com/jp/">顧客管理</a></li>
+                    <li><a href="www.yanmar.com/jp/">YANMAR</a></li>
+                    <li><a href="www.yanmar.com/jp/">YDS</a></li>
                 </ul>
             </nav>
 
