4c7f0c4fbf446ccbd71cea6400d5014f0e6b0ec9
齊藤涼太 <t_and_f.800_1500@docomo.ne.jp>
2018-07-04 20:29:03 +0900

    下記の内容修正

--- resources/views/adminlte/header.blade.php
+++ resources/views/adminlte/header.blade.php
@@ -8,7 +8,8 @@
             <nav class="navbar navbar-static-top" role="navigation">
                 <ul class="nav navbar-nav">
                     <li><a href="https://www.yanmar.com/jp/" class="nav">YANMAR</a></li>
-                    <li><a href="https://www.yanmar.com/jp/" class="nav">YDS</a></li>
+                    <li><a href="https://yds-next.yanmar.co.jp/" class="nav">YDS</a></li>
+                    <li><a href="https://www.yanmar.com/jp/" class="nav">齊敏機械HP</a></li>
                 </ul>
             </nav>
 
